6c5dbb8462d9e1a75c6d2373d19535168f51689e
Abayomi-Azeem <abayomioyaifo@gmail.com>
2022-08-11 06:34:22 +0100

    Add project files.

---
+++ Assignment6_SRP_and_OCP/TaxPayers.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6_SRP_and_OCP
+{
+
+    /// <summary>
+    /// Abstract class with only one method for generating tax
+    /// </summary>
+    public abstract class Tax
+    {
+        public abstract double GenerateTax();
+    }
+
+
+    /// <summary>
+    /// Inherits from Tax class and generates taxes for civilians  
+    /// </summary>
+    public class CivilServantsTax: Tax
+    {
+        public double Salary { get; set; }
+
+        public string Id { get; set; }
+
+        public int Level { get;  }
+
+
+
+        /// <summary>
+        /// Generates taxes for civil servants
+        /// </summary>
+        public override double GenerateTax()
+        {
+            
+                return 0.015 * Salary;
+            
+        }
+    }
+
+
+    /// <summary>
+    /// Generates Tax for Small and Medium Scale Enterprises
+    /// </summary>
+    public class SmallMediumEnterprisesTax: Tax
+    {
+        public double Income { get; set; }
+
+        public double BusinessId { get; set; }
+
+
+        /// <summary>
+        ///     /// Generates Tax for Small and Medium Scale Enterprises
+        /// </summary>
+        public override double GenerateTax()
+        {
+            return 0.02 * Income;
+        }
+    }
+
+
+    /// <summary>
+    /// Generates taxes for Large Corporations in the Fast Moving Consumer goods sector of the economy
+    /// </summary>
+    public class FMGCGCorporationsTax : Tax
+    {
+        public double Income { get; set; }
+
+        public double BusinessId { get; set; }
+
+
+        /// <summary>
+        /// Generates taxes for Large Corporations in the Fast Moving Consumer goods sector of the economy
+        /// </summary>
+        public override double GenerateTax()
+        {
+            return 0.035 * Income;
+        }
+    }
+}
---
+++ Assignment6_SRP_and_OCP/TaxRev.cs
@@ -0,0 +1,28 @@
+﻿namespace Assignment6_SRP_and_OCP
+{
+
+    /// <summary>
+    /// Calculates Total Revenue from all Tax Sources Monthly
+    /// 
+    /// </summary>
+    public class TaxRevenue
+    {
+        public double RevenueCalc(List<Tax> taxes)
+        {
+            try
+            {
+                double totalRevenue = 0;
+                foreach (var i in taxes)
+                {
+                    totalRevenue += i.GenerateTax();
+                }
+                return totalRevenue;
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine("Cannot Calculate total revenue at this time");
+                return 0;
+            }
+        }
+    }
+}
